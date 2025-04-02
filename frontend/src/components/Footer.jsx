989c28c7aff018a84da977ab529963a5961ede18
NamanHarbola <namanharbola3@gmail.com>
2025-04-02 23:34:24 +0530

    first-commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -22,10 +22,14 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <p className='text-xl font-medium mb-5'>DEVELOPERS</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+91 8178946367</li>
+            <li>Naman Harbola</li>
             <li>namanharbola3@gmail.com</li>
+            <li>+91 8178946367</li>
+            <li>Anushka Saxena</li>
+            <li>anushkasaxena0906@gmail.com</li>
+            <li> +91 9643569565</li>
           </ul>
         </div>
 
