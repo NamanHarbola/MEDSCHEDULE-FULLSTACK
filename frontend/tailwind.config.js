264ec94a2b1d41db15d22d2334c41dbfb1165e63
namanharbola3 <namanharbola3.com>
2025-04-02 18:22:46 +0530

    first-commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
+      },
+      colors: {
+        'emerald-500': '#10B981', // Changed to green (Emerald-500)
+      }
+    },
+  },
+  plugins: [],
+}
