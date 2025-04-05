a340bddbe3d85d50a28873fb14349d1dd766b5aa
NamanHarbola <namanharbola3@gmail.com>
2025-04-06 00:01:41 +0530

    first-commit

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -10,19 +10,9 @@ export default {
         'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
       },
       colors: {
-        'Emerald-500': '#10B981', // Your custom green color
+        'Emerald-500': '#10B981' // Your custom green color
       }
     },
-  },
-  plugins: [],
-}
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
   },
   plugins: [],
 }
